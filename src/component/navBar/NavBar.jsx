c119fabf4400cd8deb06c0c12293ff21008fb0d5
Duru Oluchi M <duwend46@gmail.com>
2023-11-04 19:04:29 +0100

    screen responsiveness

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -1,36 +1,51 @@
 import './navBar.css'
 import {Link} from "react-router-dom";
+import React, { useState } from "react";
+
+
 const NavBar = () =>{
+    const [isOpen,setIsOpen] = useState(false)
+    const handleClick=()=>{
+        setIsOpen(!isOpen)
+    }
 
     return(
         <div class="nav-main-container">
-          <div className="inner-container" >
-              <h4>TimeKeper</h4>
-              <div className="navBarFields">
-                 <ul>
-                   <li><a href="#">Home</a></li>
-                     <Link to="/login">Login</Link>
-                     <li><a href="#">About</a></li>
-                     <li><a href="#">Contact Us</a></li>
-                     <li><a href="#">Blog</a></li>
-                     <div className="buttons">
-                         <Link to="/sign_up">
-                         <button>
-                             Sign up</button>
-                         </Link>
-                     </div>
-                     <div className="LoginButtons">
-                         <Link to="/login">
-                             <button>Login</button>
-                         </Link>
-                     </div>
+            <div className="inner-container" >
+                <h4>TimeKeper</h4>
+                <div className="navBarFields">
+                    <ul>
+                        <li className="active"><a href="#">Home</a></li>
+                        <Link to="/login">Login</Link>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <div className="buttons">
+                            <Link to="/sign_up">
+                                <button>
+                                    Sign up</button>
+                            </Link>
+                        </div>
+                        <div className="LoginButtons">
+                            <Link to="/login">
+                                <button>Login</button>
+                            </Link>
+                        </div>
 
-                 </ul>
+                    </ul>
+                </div>
+                <div id="mobile">
+                    <i className="fasfa-bars">
+                    </i>
+                </div>
+                <div id="mobile">
+                    <i className="fasfa-times">
+                    </i>
+                </div>
 
-
-              </div>
-          </div>
+            </div>
         </div>
+
     )
 }
 export default NavBar
